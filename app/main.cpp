223c24e9b829164cb8c1353df135d35da14192ae
yorez <yorez@naver.com>
2023-04-22 16:05:07 +0900

    refactor: modoo format adjust

---
+++ app/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 김영준 on 2023/04/22.
+//
+
+#include "main.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ app/main.h
@@ -0,0 +1,14 @@
+//
+// Created by 김영준 on 2023/04/22.
+//
+
+#ifndef LEET_CODE_MAIN_H
+#define LEET_CODE_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //LEET_CODE_MAIN_H
