681066e6646870c07159c2d209941fe1887d801a
yorez <yorez@naver.com>
2023-04-23 12:52:57 +0900

    feat: 36-valid-sudoku sample

---
+++ include/36-valid-sudoku.h
@@ -0,0 +1,13 @@
+#ifndef LEETCODEPROJECT_INCLUDE_36_VALID_SUDOKU_H_
+#define LEETCODEPROJECT_INCLUDE_36_VALID_SUDOKU_H_
+
+#include <vector>
+
+namespace leetcode_36 {
+class Solution {
+ public:
+  bool isValidSudoku(std::vector<std::vector<char>> &board);
+};
+}
+
+#endif //LEETCODEPROJECT_INCLUDE_36_VALID_SUDOKU_H_
